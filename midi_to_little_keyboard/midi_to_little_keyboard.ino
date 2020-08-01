a33ab5ce521e3fe4a8a59c6aaa6bf64419f1ce08
Arvid <arvid@arvidandmarie.com>
2020-08-02 00:37:20 +0200

    fully function duophonic

--- midi_to_little_keyboard/midi_to_little_keyboard.ino
+++ midi_to_little_keyboard/midi_to_little_keyboard.ino
@@ -1,42 +1,62 @@
 #define  GPIO2_PREFER_SPEED  1
 #include <DIO2.h>
 
-int note = 0;
+#include <MIDI.h>
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+int note[] = {-1,-1};
+bool lastnote = 0;
 
 long timer = 0;
 long delayT = 1000;
 
+int midiOffset = 41;//the midi note which the lowest key corresponds to
 
 
 void setup() {
-  Serial.begin(9600);
   for (int scan = 10; scan >= 3; scan--) {
     pinMode2(scan, INPUT);
   }
+  MIDI.setHandleNoteOn(handleNoteOn);
+  MIDI.setHandleNoteOff(handleNoteOff);
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  Serial.begin(115200);
   timer = millis() + delayT;
 }
 
 void loop() {
+  MIDI.read();
+
   if (timer < millis()) {
     timer = millis() + delayT;
-    note = (note + 1) % 32 ;
     digitalWrite2(13, !digitalRead2(13));
-    Serial.println(note);
-
   }
 
-  //if (digitalRead(21) && digitalRead(20) && digitalRead(19))Serial.println("trigg");
-
-    for (int scan = 0; scan < 8; scan++) {//30==14 , 23==21
-      if (digitalRead2(scan + 14)) {
-        for (int read = 0; read <= 8; read++) {//11=3 , 18=10
-  
-  
-          if (read + scan  * 8 + 1 == note + 1) {
-            digitalWrite2(read + 3, HIGH);
-          }
-          else pinMode2(read + 3, INPUT);
+  for (int scan = 0; scan < 8; scan++) {//30==14 , 23==21
+    if (digitalRead2(scan + 14)) {
+      for (int read = 0; read <= 8; read++) {//11=3 , 18=10
+        if (read + scan  * 8  == note[0]  || read + scan  * 8 == note[1] ) {
+          digitalWrite2(read + 3, HIGH);
         }
+        else pinMode2(read + 3, INPUT);
       }
     }
+  }
+}
+
+void handleNoteOn(byte channel, byte pitch, byte velocity)
+{
+  if (pitch >= midiOffset && pitch < midiOffset + 32) {
+    note[lastnote] = pitch - midiOffset;
+    lastnote=!lastnote;
+  }
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity)
+{
+  if (pitch >= midiOffset && pitch < midiOffset + 32) {
+    if(note[0] == pitch - midiOffset)note[0]=-1;
+    if(note[1] == pitch - midiOffset)note[1]=-1;
+  }
 }
